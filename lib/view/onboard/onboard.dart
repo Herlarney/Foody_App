e4286e397ade3355297c87e05d7ab905a8ecfd87
herlarney <sradecoration@gmail.com>
2023-03-03 02:26:51 -0800

    first commit

---
+++ lib/view/onboard/onboard.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Onboard extends StatelessWidget {
+  const Onboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(image: AssetImage('images/imaged.png'))),
+          child: Column(
+            children: [
+              Text('')
+            ],
+          )
+    );
+  }
+}
